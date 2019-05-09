f952a61cbd36b13fa17c2aaa3d27045e0a64acf9
Kaaazmi <39811688+Kaaazmi@users.noreply.github.com>
2019-05-09 18:31:56 +0500

    Updates

---
+++ precache-manifest.967a06372b571121f1c2866a23366dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d337e2e2d37897a74c33",
+    "url": "/smartbrainapp/static/js/runtime~main.a595c7da.js"
+  },
+  {
+    "revision": "98ac1ee4cb7454ec9860",
+    "url": "/smartbrainapp/static/js/main.d33a9f51.chunk.js"
+  },
+  {
+    "revision": "a0f1e085a8ca963e9d50",
+    "url": "/smartbrainapp/static/js/2.343b017c.chunk.js"
+  },
+  {
+    "revision": "98ac1ee4cb7454ec9860",
+    "url": "/smartbrainapp/static/css/main.e8607b1b.chunk.css"
+  },
+  {
+    "revision": "a0f1e085a8ca963e9d50",
+    "url": "/smartbrainapp/static/css/2.00a6930f.chunk.css"
+  },
+  {
+    "revision": "e169ccf5ea2bfdcee67db5438f483a61",
+    "url": "/smartbrainapp/index.html"
+  }
+];
